109118dfa98466fbc2695407e4348540f5653b24
VicenteA18UCN <105085389+VicenteA18UCN@users.noreply.github.com>
2023-12-17 18:05:14 -0300

    fix: fixed regular expression in email

--- Backend/MobileHub/Src/Common/RegularExpression.cs
+++ Backend/MobileHub/Src/Common/RegularExpression.cs
@@ -4,11 +4,10 @@ namespace MobileHub.Src.Common
 {
     public static partial class RegularExpressions
     {
-        [GeneratedRegex("^([a-zA-Z]+\\.)*ucn\\.cl$", RegexOptions.Compiled)]
-        public static partial Regex UCNEmailDomainRegex();
+        [GeneratedRegex("^(disc\\.ucn\\.cl|ce\\.ucn\\.cl|alumnos\\.ucn\\.cl|ucn\\.cl)$", RegexOptions.Compiled)] public static partial Regex UCNEmailDomainRegex();
     }
 
-        public static partial class RegularExpressions
+    public static partial class RegularExpressions
     {
         [GeneratedRegex("^([0-9]+-[0-9K])$", RegexOptions.Compiled)]
         public static partial Regex RutRegex();
